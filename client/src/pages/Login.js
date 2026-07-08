0962fad93c585d5e3b6db558cfc89e0965af6d6d
Gayatri Nayakwadi <gayatrinayakwadi@gmail.com>
2026-07-08 10:58:09 +0530

    Rename Login css final

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
-import "../styles/LoginTemp.css";
+import "../styles/Login.css";
 
 import {
   FaEnvelope,
