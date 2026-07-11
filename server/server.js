ab3af6020a2704912cbf938172e6cccf5bbe54ac
Gayatri Nayakwadi <gayatrinayakwadi@gmail.com>
2026-07-11 11:08:46 +0530

    Add Authority API and auth_type support

--- server/server.js
+++ server/server.js
@@ -762,6 +762,7 @@ app.post("/authority", async (req, res) => {
       department,
       designation,
       authority_level,
+      auth_type,
       joining_date,
       status,
 
@@ -789,6 +790,7 @@ employee_id,
 department,
 designation,
 authority_level,
+auth_type,
 joining_date,
 status,
 
@@ -808,13 +810,12 @@ remarks
 
 VALUES(
 
-$1,$2,$3,$4,$5,$6,$7,$8,
-$9,$10,$11,
-$12,$13,$14,$15,$16,
-$17
+$1,$2,$3,$4,$5,$6,$7,$8,$9,
+$10,$11,$12,
+$13,$14,$15,$16,$17,
+$18
 
 )
-
 RETURNING *`,
 
 [
@@ -824,6 +825,7 @@ employee_id,
 department,
 designation,
 authority_level,
+auth_type,
 joining_date,
 status,
 
